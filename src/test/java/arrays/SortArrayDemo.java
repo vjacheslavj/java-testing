9888e0f56516613d4ac3e52f6bef2c3e042e8b4b
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2023-06-27 19:19:22 +0300

    Initial Commit

---
+++ src/test/java/arrays/SortArrayDemo.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class SortArrayDemo {
+    public static void main(String[] args) {
+        int a[] = {2, 6, 1, 4, 9};
+        int temp;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+
+}
---
+++ src/test/java/arrays/SumOfAllTheElementInArray.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class SumOfAllTheElementInArray {
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4, 5};
+
+        int sum = sumArray(a);
+        System.out.println(sum);
+    }
+
+    public static int sumArray(int[] a) {
+
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+        }
+        return sum;
+    }
+}
