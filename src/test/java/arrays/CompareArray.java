cce3a6dfe2da09aaaba5636b4eb3a416ce2b0b45
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2023-07-11 17:39:51 +0300

    New class - where you can learn how to swap the variable with and without using temp variable

--- src/test/java/arrays/CompareArray.java
+++ src/test/java/arrays/CompareArray.java
@@ -3,15 +3,15 @@ package arrays;
 import java.util.ArrayList;
 
 public class CompareArray {
-//compare same indexes of 2 different arrays and create another array for matching values
+    //compare same indexes of 2 different arrays and create another array for matching values
     public static void main(String[] args) {
 
-        int a[] = {1,4,5,7};
-        int b[] = {6,4,3,7};   //{4,7}
-      //  int[] ab = new int[4];
-        ArrayList<Integer> al = new  ArrayList<Integer>();
+        int a[] = {1, 4, 5, 7};
+        int b[] = {6, 4, 3, 7};   //{4,7}
+        //  int[] ab = new int[4];
+        ArrayList<Integer> al = new ArrayList<Integer>();
 
-        for(int i=0; i<a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             if (a[i] == b[i]) {
                 //code to create another array
                 al.add(a[i]);
---
+++ src/test/java/arrays/SwapTheVariableWithAndWithoutUsingTempVariable.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class SwapTheVariableWithAndWithoutUsingTempVariable {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4;
+        int temp;
+        // Basket A apple
+        // Basket B orange
+        //Basket C apple
+        /**temp=a;
+         a=b;
+         b=temp;
+         System.out.println(a);
+         System.out.println(b); */
+
+        //Swap without variable
+
+        a = a + b; //a = 9
+        b = a - b; //b = 5
+        a = a - b; //a = 4
+
+    }
+}
