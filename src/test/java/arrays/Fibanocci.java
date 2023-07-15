121f10784c4dc915295225112e17eaf5c9a91185
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2023-07-15 12:14:43 +0300

    New class - where you can learn how to print fibanocci series by swapping variables

---
+++ src/test/java/arrays/Fibanocci.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Fibanocci {
+
+    public static void main(String[] args) {
+        //0, 1, 1 2 3 5 8 13 21 34
+        //0 - a
+        //1 - b  /a
+        //1 - sum sum/b
+
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        int i = 1;
+        while (i<9) {
+            sum = a+b;
+            System.out.println(sum);
+            a=b;
+            b=sum;
+            i++;
+        }
+
+    }
+}
