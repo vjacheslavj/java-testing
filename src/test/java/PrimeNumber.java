e73a95f82eef0da17a671da7f04d70600ae7fdcd
vjaceslavsj <vjaceslavs.jermakovs@bta.lv>
2023-07-25 17:22:46 +0300

    New class - where you can find how to check if given number is prime number

---
+++ src/test/java/PrimeNumber.java
@@ -0,0 +1,22 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        //11%i=0
+
+        boolean flag = false;
+        int val = 7;
+
+        for (int i=2; i * i < val; i++) {
+            if(val%i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println("it is not prime");
+        } else {
+            System.out.println("it is prime");
+        }
+    }
+}
