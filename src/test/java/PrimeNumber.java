41be36c7d8caa6d0f6d3e25e1f3d4d3ef518effd
vjaceslavsj <vjaceslavs.jermakovs@bta.lv>
2025-01-30 10:20:48 +0200

    Some changes

--- src/test/java/PrimeNumber.java
+++ src/test/java/PrimeNumber.java
@@ -4,9 +4,9 @@ public class PrimeNumber {
         //11%i=0
 
         boolean flag = false;
-        int val = 4;
+        int val = 8;
 
-        for (int i=2; i * i < val; i++) {
+        for (int i=2; i * i <= val; i++) {
             if(val%i == 0) {
                 flag = true;
                 break;
--- src/test/java/ReverseString.java
+++ src/test/java/ReverseString.java
@@ -2,15 +2,15 @@ public class ReverseString {
 
     public static void main(String[] args) {
 
-        String s = "Rahul";
-        String t = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            t = t + s.charAt(i);
-        }
-        System.out.println(t);
+        int a = 5;
+        int b = 4;
+        int temp;
 
-        if (s == t) {
-            System.out.println("it is palindrome");
-        }
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
     }
 }
